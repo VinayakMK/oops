04eccaa28aab453b11f6e2e26e9dbce83cb7c3e0
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-20 23:57:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+/*var arr = [10,20,30];
+
+function abc(){
+    console.log("This is a function");
+}
+
+abc();*/
+
+
+
+
+/*class User{
+
+    constructor(name){
+        this.name = name
+    }
+
+    display(){
+        alert('Welcome ${this.name}');
+    }
+}
+
+let obj = new User("Jack");
+obj.display();*/
+
+
+
+class User{
+    setName(name){
+        this.name - name;
+    }
+
+    getName(){
+        return this.name;
+    }
+}
+
+let obj = new User();
+let obj1 = new User();
+
+obj.setName
+obj.setName
+
+console.log
